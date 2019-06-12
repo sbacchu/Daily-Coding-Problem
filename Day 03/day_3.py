46676f4223fb32a85b6b3e2f1498a74ce8b4e366
Suhas Bacchu <suhas.bacchu29@gmail.com>
2019-06-11 23:53:14 -0500

    Finished Serialize part

--- Day 03/day_3.py
+++ Day 03/day_3.py
@@ -5,16 +5,37 @@ class Node:
         self.right = right
 
 
-def serialize(node: Node):
+# Answer was
+# root l e f t   l   e   f   t   .   l   e   f   t       -       1       -       1   -   1 r i g h t   -   1   -   1
+# Because the return is a string
+# def serialize(node: Node):
+#     preorder_traversal = []
+#     if (node is not None):
+#         preorder_traversal.append(node.val)
+#         preorder_traversal.extend(serialize(node.left))
+#         preorder_traversal.extend(serialize(node.right))
+
+#     if (node is None):
+#         preorder_traversal.append(-1)
+
+#     return " ".join(str(x) for x in preorder_traversal)
+
+
+def preorder_traverse(node: Node):
     preorder_traversal = []
     if (node is not None):
         preorder_traversal.append(node.val)
-        preorder_traversal.extend(serialize(node.left))
-        preorder_traversal.extend(serialize(node.right))
+        preorder_traversal.extend(preorder_traverse(node.left))
+        preorder_traversal.extend(preorder_traverse(node.right))
 
     if (node is None):
         preorder_traversal.append(-1)
 
+    return preorder_traversal
+
+
+def serialize(node: Node):
+    preorder_traversal = preorder_traverse(node)
     return " ".join(str(x) for x in preorder_traversal)
 
 

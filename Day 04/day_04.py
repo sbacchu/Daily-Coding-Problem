01fa4d644e2e49ca2cbc884711a2c1fbc6976a80
Suhas Bacchu <suhas.bacchu29@gmail.com>
2019-06-17 00:25:04 -0500

    Implemted the solution

--- Day 04/day_04.py
+++ Day 04/day_04.py
@@ -1,19 +1,30 @@
-def solution(arr):
+from numpy import array
 
-    hash_arr = []
 
-    for elem in arr:
-        if elem > 0:
-            hash_arr[elem] = elem
+def solution(list):
+    # First remove all negative numbers
+    list[:] = [x for x in list if x > 0]
 
-    return hash_arr
+    # keep track of numbers using negatives
+    list_len = len(list)
+    for elem in list:
+        elem_abs = abs(elem)
+        if (elem_abs > 0 and elem_abs < list_len):
+            # To take care of duplicate numbers
+            list[elem_abs] = -abs(list[elem_abs])
+
+    for x, val in enumerate(list):
+        if val > 0 and x != 0:
+            return x
+
+    return list_len + 1
 
 
 def main():
     # test_list = [3, 4, -1, 1]
     # test_list = [1, 2, 0]
-    test_list = [2, 4, -8, 10, 15, 0, 0, -1, 1]
-    # test_list = [0, 0, 0]
+    # test_list = [2, 4, -8, 10, 15, 0, 0, -1, 1]
+    test_list = [0, 0, 0]
     print(solution(test_list))
 
 
